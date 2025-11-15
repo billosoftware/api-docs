fc252df56991400b2bb4f626ff7ba8edda633108
Luke Orriss <73408132+v0sudo@users.noreply.github.com>
2025-11-15 22:06:26 +0000

    Enhance footer layout for better responsiveness and add 'Main App' link to navbar

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,16 +5,19 @@ import { CommandIcon, FavouriteIcon, NewTwitterIcon, Triangle01Icon } from "@hug
 
 export function Footer() {
   return (
-    <footer className="border-t w-full h-16">
+    <footer className="border-t w-full sm:h-16">
       <div className="container flex items-center sm:justify-between justify-center sm:gap-0 gap-4 h-full text-muted-foreground text-sm flex-wrap sm:py-0 py-3 max-sm:px-4">
-        <div className="flex items-center gap-3">
+        <div className="flex flex-col sm:flex-row items-center gap-3">
           <HugeiconsIcon icon={CommandIcon} className="sm:block hidden w-5 h-5 text-muted-foreground" />
           <p className="text-center">
             Built by{" "}
             <Link className="px-1 underline underline-offset-2" href="https://ldo.dev">
               v0sudo
             </Link>
-            . The source code is available on{" "}
+            .
+          </p>
+          <p className="text-center">
+            The source code is available on{" "}
             <Link className="px-1 underline underline-offset-2" href="https://github.com/billosoftware/api-docs">
               GitHub
             </Link>
